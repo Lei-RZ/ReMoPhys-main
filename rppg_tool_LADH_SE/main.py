7976f76911a6987b95ec6c4174baaa1f36395015
Lei-RZ <18202722449@163.com>
2026-08-21 17:18:17 +0800

    Update main.py

--- rppg_tool_LADH_SE/main.py
+++ rppg_tool_LADH_SE/main.py
@@ -227,7 +227,7 @@ if __name__ == "__main__":
             valid_loader = data_loader.SUMSLoader.SUMSLoader
         elif config.VALID.DATA.DATASET == "LADH":
             valid_loader = data_loader.LADHLoader.LADHLoader
-        elif config.VAILD.DATA.DATASET == "LADH-SE":
+        elif config.VAILD.DATA.DATASET == "LADH+":
             vaild_loader = data_loader.LADHLoader.LADHLoader
         elif config.VALID.DATA.DATASET is None and not config.TEST.USE_LAST_EPOCH:
             raise ValueError("Validation dataset not specified despite USE_LAST_EPOCH set to False!")
@@ -279,7 +279,7 @@ if __name__ == "__main__":
             test_loader = data_loader.SUMSLoader.SUMSLoader
         elif config.TEST.DATA.DATASET == "LADH":
             test_loader = data_loader.LADHLoader.LADHLoader
-        elif config.TEST.DATA.DATASET == "LADH-SE":
+        elif config.TEST.DATA.DATASET == "LADH+":
             test_loader = data_loader.LADHLoader.LADHLoader
         else:
             raise ValueError("Unsupported dataset! Currently supporting UBFC-rPPG, PURE, MMPD, \
@@ -326,7 +326,7 @@ if __name__ == "__main__":
             unsupervised_loader = data_loader.SUMSLoader.SUMSLoader
         elif config.UNSUPERVISED.TRAIN.DATA.DATASET == "LADH":
             unsupervised_loader = data_loader.LADHLoader.LADHLoader
-        elif config.UNSUPERVISED.TRAIN.DATA.DATASET == "LADH-SE":
+        elif config.UNSUPERVISED.TRAIN.DATA.DATASET == "LADH+":
             unsupervised_loader = data_loader.LADHLoader.LADHLoader
         else:
             raise ValueError("Unsupported dataset! Currently supporting UBFC-rPPG, PURE, MMPD, \
